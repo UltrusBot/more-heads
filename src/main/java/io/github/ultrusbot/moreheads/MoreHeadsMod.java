50c25c51bb6751f4f4033f0ea39909447899c74a
ultra <13109003+UltrusBot@users.noreply.github.com>
2021-06-23 00:03:25 -0400

    setup project

---
+++ src/main/java/io/github/ultrusbot/moreheads/MoreHeadsMod.java
@@ -0,0 +1,10 @@
+package io.github.ultrusbot.moreheads;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MoreHeadsMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
